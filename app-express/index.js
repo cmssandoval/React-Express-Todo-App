75b6227f39245b76e864c03cffe5508265327599
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-08-27 15:02:31 -0400

    Add delete method, fix put method, add getTodos method, update imports
    > Add delete method with put previous method.
    > Fix put method, it has the delete method instead. The actual method uses a map to toggle todo done state.
    > Import and use cors to enable cross-origin resource sharing for future uses of the server from another domains.
    > Limit fs imports with destructuring, also switched to fs/promises for async/await use.
    > Minor comments fix.
    
    Related to the reference project:
    > Personally decided to use crypto.randomUUID instead of Nano ID to reduce dependencies.
    > Also, declared each todos array as CONST instead of using LET, for anticipated data integrity protection.
    > Todos array get updated with .map and .filter by assigning the result to 'updatedTodos'.

--- app-express/index.js
+++ app-express/index.js
@@ -1,6 +1,7 @@
 // Imports
 import express from 'express';
-import fs from 'fs/promises';
+import { writeFile, readFile } from 'fs/promises';
+import cors from 'cors';
 
 // Settings
 const app = express();
@@ -8,35 +9,39 @@ const port = 5000;
 
 // Middleware calls
 app.use(express.json());
+app.use(cors());
 
-// Server listening inicilization
+// Server listening initilization
 app.listen( port, () => {
     console.log(`¡Server is on! Listening on port ${ port }`);
     console.log(`Go to http://localhost:${ port }/`);
 });
 
-// Data
-const todos = [
-    { id: 1, title: "Todo 1", done: false },
-    { id: 2, title: "Todo 2", done: false },
-    { id: 3, title: "Todo 3", done: false },
-];
+// Read and parse array of todos from todos.json
+const getTodos = async () => {
+    const fsResponse = await readFile('./todos.json', 'utf-8');
+    const todos = JSON.parse( fsResponse )
+    return todos;
+};
 
-// GET routes
-app.get('/todos', ( req, res ) => {
+// GET ROUTES
+app.get('/todos', async ( req, res ) => {
+    const todos = await getTodos();
     res.json( todos );
 });
 
-app.get('/todos/:id', ( req, res ) => {
-    const id    = Number(req.params.id);
+app.get('/todos/:id', async ( req, res ) => {
+    const id    = req.params.id;
+    
+    const todos = await getTodos();
     const todo  = todos.find( todo => todo.id === id );
 
-    if( !todo ) res.status(404).json({ message: "Todo not found" });
+    if( !todo ) res.status(404).json({ message: 'Todo not found' });
     res.json( todo );
 });
 
-// POST routes
-app.post('/todos', ( req,res ) => {
+// POST METHOD
+app.post('/todos', async ( req,res ) => {
     const { title } = req.body;
 
     const newTodo = {
@@ -45,23 +50,43 @@ app.post('/todos', ( req,res ) => {
         done: false,
     };
 
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
+    const todos = await getTodos();
+    todos.push( newTodo );
+
+    await writeFile('./todos.json', JSON.stringify( todos, null, 4 ));
+
+    res.status(201).json( newTodo );
 });
 
-// PUT routes
+// PUT METHOD
 app.put('/todos/:id', async ( req, res ) => {
-    const id = Number(req.params.id);
-    
-    //! CREATE getTodos function and refactorize
-    //* todos will be readed from a json file. Delete test array.
-    const todos = await getTodos(); 
-    const todo = todos.find( todo => todo.id === id);
+    const id = req.params.id;
 
-    if( !todo ) res.status(404).json({ message: "Todo not found" });
+    const todos = await getTodos(); 
+    const todo = todos.find( todo => todo.id === id );
+
+    if( !todo ) res.status(404).json({ message: 'Todo not found' });
+
+    const updatedTodos = todos.map( todo => {
+        if ( todo.id === id ) return { ...todo, done: !todo.done };
+        return todo;
+    });
+
+    await writeFile('./todos.json', JSON.stringify( updatedTodos, null, 4 ));
+    res.json( updatedTodos );
+});
+
+// DELETE METHOD
+app.delete('/todos/:id', async ( req, res ) => {
+    const id = req.params.id;
+
+    const todos = await getTodos();
+    const todo = todos.find( todo => todo.id === id );
+
+    if ( !todo ) res.status(404).json({ message: 'Todo not found' });
 
     const updatedTodos = todos.filter( todo => todo.id !== id );
 
-    await fs.writeFile('./todos.json', JSON.stringify( updatedTodos, null, 4));
-    res.json(updatedTodos);
+    await writeFile('./todos.json', JSON.stringify( updatedTodos, null, 4 ));
+    res.json( updatedTodos );
 });
